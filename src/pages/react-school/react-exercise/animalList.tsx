90bd90bf12b6526f1e0d32a24ac846b83466af4a
Mahdi <mahdi.mousavi0011@gmail.com>
2025-02-21 12:15:23 +0100

    fix: animlList-added resetbtn, alert for empty input.

--- src/pages/react-school/react-exercise/animalList.tsx
+++ src/pages/react-school/react-exercise/animalList.tsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 
 export default function AnimalList() {
-  const [animals, setAnimals] = useState<string[]>(["gorilla", "giraff"]);
+  const [animals, setAnimals] = useState<string[]>(["gorilla", "giraff","Lion"]);
   const [inputAnimal, setInput] = useState("");
 
   const deleteAnimal = (animal: string) => {
     setAnimals(animals.filter((a) => a !== animal));
   };
   const editAnimal = (animal: string) => {
+    if (!inputAnimal) {
+      alert("We miss your animal name!")
+      return
+    }
     setAnimals(animals.map((a) => (a === animal ? inputAnimal : a)));
+    setInput("");
   };
-  const handleClick = () => {
+  const addAnimal = () => {
+    if (!inputAnimal) {
+      alert("We miss your animal name!")
+      return
+    }
     setAnimals([...animals, inputAnimal]);
+    setInput("");
+  };
+  const reset = () =>{
+    setAnimals(["gorilla", "giraff","Lion"]);
   };
 
   return (
@@ -20,11 +33,13 @@ export default function AnimalList() {
       <input
         type="text"
         value={inputAnimal}
+        placeholder="Write your animal..."
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           setInput(e.target.value);
         }}
       />
-      <button onClick={handleClick}>Add Animal</button>
+      <button onClick={addAnimal}>Add Animal</button>
+      <button onClick={reset}>Reset</button>
       <ul>
         {animals.map((animal) => (
           <li key={animal}>
